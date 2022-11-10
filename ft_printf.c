40b2f1e46dbdcb1e24ffc9d5feeb59e70ef59b5f
gozturk <glserozkan@gmail.com>
2022-11-10 18:34:55 +0100

    step by step

--- ft_printf.c
+++ ft_printf.c
@@ -1,39 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gozturk <marvin@codam.nl>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/10 18:05:45 by gozturk       #+#    #+#                 */
+/*   Updated: 2022/11/10 18:34:20 by gozturk       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
-int	ft_printf(const char *str, ...)
+int    ft_putchar_fd(char c)
 {
-//	const char str[100];
-	va_list	args;
-	va_start(args, str);
-//	int	len;
+    write(1, &c, 1);
+	return (1);
+}
+
+static int	convert_format(int sp, va_list arg)
+{	
+	if (sp == 'c')
+			return(ft_putchar_fd(va_arg(arg, int)));
+//	if (sp == '%')
+}
+
+int	ft_printf(const char *fstr, ...)
+{
+	va_list	arg;
+	int c;
 	int	i;
-	char c;
+	int	len;
 	
 	i = 0;
-//	len = strlen(str);
-	while (*str != '\0')
+	va_start(arg, fstr);
+	while (fstr[i] != '\0')
 	{
-		if (*str == '%')
+		if (fstr[i] == '%')
 		{
-			str++;
+			i++;
+			c = convert_format(fstr[i], arg);
 		}
-		else if (*str == 'c')
-		{
-			c = va_arg(args, char);
-			write(1, &c, 1);
-		}
-	//	i++;
+		else if (fstr[i] != '%')
+			c = ft_putchar_fd(fstr[i]);
 	}
-	va_end(args);
-	return 0;
+	va_end(arg);
+	return (c);
 }
 
 int main()
-{		
-	char my_char = 'k';
-	ft_printf("%c", my_char);
-//	printf("\n%c", my_char);
+{	
+	int	i;
+
+	i = printf("hello");
+	printf("\n i = %d", i);	
+	//ft_printf("%c", 'm');
+//	printf("\n%c", 'm');
+//
 }
