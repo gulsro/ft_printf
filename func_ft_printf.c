d500f84e290670050cb85ad8ec5f79eec4aeb9cc
gulser <glserozkan@gmail.com>
2022-11-12 21:29:53 +0100

    better

--- func_ft_printf.c
+++ func_ft_printf.c
@@ -16,27 +16,22 @@ int     ft_putnbr(int i)
 	if (n < 0)
 	{
 		n = n * -1;
-		len += write(1, "-", 1);
+		write(1, "-", 1);
+		len++;
 	}
 	if (n > 9)
 	{
-//		len += ft_putnbr(n/10);
-		ft_putnbr(n/10);
+		len += ft_putnbr(n/10);
 	}
 	n = n % 10;
 	n = n + '0';
-	len++;
-	write(1, &n, 1);
-//	return(write(1, &n, 1));
-//	return (len + write(1, &n, 1));
+	len += write(1, &n, 1);
 	return (len);
 }
 
 int main()
 {
-//	int a;
 	int i = -6543;
-//	a = ft_putnbr(i);
 	printf("\n%d\n", ft_putnbr(i));
 	ft_putnbr(i);
 }
