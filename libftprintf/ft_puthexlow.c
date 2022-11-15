5499cdb85d06328b4a22a86b05b2bb68488dabeb
gulser <glserozkan@gmail.com>
2022-11-15 22:19:01 +0100

    updeeeyt

--- libftprintf/ft_puthexlow.c
+++ libftprintf/ft_puthexlow.c
@@ -14,17 +14,15 @@
 
 int	ft_puthexlow(unsigned long i)
 {
-	int	len;
-	int	arr[16];
-	int	remainder;
+        int len;
 
-	remainder = 0;
-	arr[16] = {0, 1, 2, 3, 4, 5, 6, 7,
-		8, 9, a, b, c, d, e, f};
-	len = 0;
-	if (i > 15)
-	len +=  ft_puthexlow(i / 16);
-	remainder = i % 16;
-	i = arr[remainder - 1];;
-	return (len + write(1, &i, 1));
+        len = 0;
+        if (i > 15)
+                len +=  ft_puthexlow(i / 16);
+        i = i % 16;
+        if (i < 10)
+                i += 48;
+        else
+                i += 'a' - 10;
+        return (len + write(1, &i, 1));
 }
